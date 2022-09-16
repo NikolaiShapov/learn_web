19b5929623e29b56c5f6d904487a4ff81142aea9
unknown <nikolius@inbox.ru>
2022-09-16 17:50:35 +0300

    Trek Web lesson4

---
+++ python_org_news.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_html(url):
+    try:
+        result = requests.get(url)
+        result.raise_for_status()
+        return result.text
+    except (requests.RequestException, ValueError):
+        print('Erro Ethernet!')
+        return False
+
+def get_python_news():
+    html = get_html('https://www.python.org/blogs/')
+    if html:
+        soup = BeautifulSoup(html, 'html.parser')
+        all_news = soup.find('ul', class_="list-recent-posts menu").findAll('li')
+        result_news = []
+        for news in all_news:
+            title = news.find('a').text
+            url = news.find('a')['href']
+            published = news.find('time').text
+            result_news.append({
+                'title': title,
+                'url': url,
+                'published': published
+            })
+        return result_news
+    return False
