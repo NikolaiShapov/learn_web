2a3bcfc1c30a296ea99b640b2916588d6f5d3e24
unknown <nikolius@inbox.ru>
2022-09-16 17:49:35 +0300

    lesson 4 Trek web

--- server.py
+++ server.py
@@ -1,17 +1,19 @@
 from distutils.log import debug
 from re import T
-from flask import Flask
+from flask import Flask, render_template
 from weather import weather_by_city
-
+from python_org_news import get_python_news
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    title = 'Новости Python'
     weather = weather_by_city('Калуга')
-    if weather:
-        return f"Погода: {weather['temp_C']}, ощущается как {weather['FeelsLikeC']}"
-    else:
-        return 'Server weather no connect!'
+    news_list = get_python_news()
+    return render_template('index.html',
+                            page_title = title,
+                            weather_text = weather,
+                            news_list = news_list)
 
 if __name__ == '__main__':
     app.run(debug = True) # Режим Дебагер
--- weather.py
+++ weather.py
@@ -1,25 +1,30 @@
 import requests
-import setting
+import settings
 
 #http://api.worldweatheronline.com/premium/v1/weather.ashx?key=c9af2cee1cb24c99987113909221209&q=Moscow,Russia&format=json&num_of_days=1&lang=ru
 def weather_by_city(city_name):
     weather_url = 'http://api.worldweatheronline.com/premium/v1/weather.ashx'
     params = {
-        'key': setting.API_KEY,
+        'key': settings.API_KEY,
         'q': city_name,
         'format': 'json',
         'num_of_days': 1,
         'lang': 'ru'
     }
-    result = requests.get(weather_url,params=params)
-    weather = result.json()
-    if 'data' in weather:
-        if 'current_condition' in weather['data']:
-            try:
-                return weather['data']['current_condition'][0]
-            except (IndexError, TypeError):
-                return False
-    return weather
+    try:
+        result = requests.get(weather_url,params=params)
+        result.raise_for_status()
+        weather = result.json()
+        if 'data' in weather:
+            if 'current_condition' in weather['data']:
+                try:
+                    return weather['data']['current_condition'][0]
+                except (IndexError, TypeError):
+                    return False
+    except (requests.RequestException, ValueError):
+        print('Erro Ethernet.')
+        return False
+
 
 if __name__ == '__main__':
     w = weather_by_city('Kalyga')
